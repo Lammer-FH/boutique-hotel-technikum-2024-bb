23bb4df7cf3bd42d8ad150f270153df7e9c843f9
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-05-22 22:12:57 +0200

    added main class

---
+++ boutique-hotel-technikum/src/main/java/BoutiqueHotelTechnikum.java
@@ -0,0 +1,19 @@
+import Controller.BookingController;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BoutiqueHotelTechnikum {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoutiqueHotelTechnikum.class, args);
+        BookingController controller = new BookingController();
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+}
