43fe17f723daf7ad4a3ac6b2662be29779373335
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-05-25 00:30:36 +0200

    renamed ModelMapper

--- boutique-hotel-technikum/src/main/java/BoutiqueHotelTechnikum.java
+++ boutique-hotel-technikum/src/main/java/BoutiqueHotelTechnikum.java
@@ -13,7 +13,7 @@ public class BoutiqueHotelTechnikum {
     }
 
     @Bean
-    public ModelMapper getModelMapper() {
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
