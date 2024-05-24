edc27d31dd83c266021537c7fea076b992ba18ea
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-05-25 00:33:48 +0200

    added Room Endpoint

--- boutique-hotel-technikum/src/main/java/Repository/RoomRepository.java
+++ boutique-hotel-technikum/src/main/java/Repository/RoomRepository.java
@@ -2,6 +2,8 @@ package Repository;
 
 import Model.Room;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends CrudRepository<Room, Integer> {
 }
---
+++ boutique-hotel-technikum/src/main/java/Service/RoomService.java
@@ -0,0 +1,46 @@
+package Service;
+
+import Model.Room;
+import Repository.RoomRepository;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoomService {
+    @Autowired
+    private RoomRepository roomRepository;
+
+    public Room createRoom(Room room) {
+        return roomRepository.save(room);
+    }
+
+    public Room updateRoom(Room room) throws NotFoundException {
+        Optional<Room> optionalRoom = roomRepository.findById(room.getId());
+        if (optionalRoom.isPresent()) {
+            Room existingRoom = optionalRoom.get();
+            existingRoom.setTitle(room.getTitle());
+            existingRoom.setDescription(room.getDescription());
+            existingRoom.setPrice(room.getPrice());
+            existingRoom.setRoom_type(room.getRoom_type());
+            existingRoom.setBed_type(room.getBed_type());
+            existingRoom.setBed_count(room.getBed_count());
+            existingRoom.setImage(room.getImage());
+            return roomRepository.save(existingRoom);
+        } else {
+            throw new NotFoundException("Room not found with id: " + room.getId());
+        }
+    }
+
+    public Room getRoomById(int id) throws NotFoundException {
+        return roomRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Room not found with id: " + id));
+    }
+
+    public List<Room> getAllRooms() {
+        return (List<Room>) roomRepository.findAll();
+    }
+}
