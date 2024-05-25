553e2f143383ec7a0d0046e737a69980aa9161b4
sem-afor <se23m047@technikum-wien.at>
2024-05-25 18:10:12 +0200

    first steps room component

--- boutique-hotel-technikum-vue/src/App.vue
+++ boutique-hotel-technikum-vue/src/App.vue
@@ -1,25 +1,41 @@
 <template>
-  <HelloWorld msg="Welcome to Hotel Boutique!"/>
+  <div class="bg-gray-600 text-white w-1/2 m-auto">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4">All Rooms</h1>
+    </div>
+    <HelloWorld msg="Welcome to Hotel Boutique!"/>
+    <br><br><br>
+
+    <p>Verfügbare Zimmer: {{ zimmerListe.length}}</p>
+    <div v-for="room in zimmerListe" :key="room.id">
+      <RoomDetails :roomObject="room" ></RoomDetails>
+    </div>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import RoomDetails from "@/components/RoomDetails.vue";
 
 export default {
   name: 'App',
   components: {
+    RoomDetails,
     HelloWorld
+  },
+  data() {
+    return {
+      zimmerListe: [
+        {id: 1, beds: 4},
+        {id: 2, beds: 2},
+        {id: 3, beds: 3},
+        {id: 4, beds: 4},
+        {id: 5, beds: 2},
+        {id: 6, beds: 41},
+        {id: 7, beds: 41},
+      ]
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ boutique-hotel-technikum-vue/src/components/RoomDetails.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+  name: 'RoomDetails',
+  props: ["roomObject"],
+  data() {
+    return {
+      room: this.roomObject
+    }
+  },
+  methods: {}
+}
+</script>
+
+<template>
+  <div class="flex justify-between py-2 px-2 items-center">
+    <p>Id: {{room.id}}</p>
+    <p>Number of beds: {{ room.beds}}</p>
+    <button class="bg-green-300 py-1 px-1 rounded">book room</button>
+  </div>
+</template>
