34956cfbc24e7fc9a91007f487e75e5f58f493b4
sem-afor <se23m047@technikum-wien.at>
2024-05-25 19:13:25 +0200

    last first steps

--- boutique-hotel-technikum-vue/src/App.vue
+++ boutique-hotel-technikum-vue/src/App.vue
@@ -5,10 +5,19 @@
     </div>
     <HelloWorld msg="Welcome to Hotel Boutique!"/>
     <br><br><br>
+    <div class="mt-4">
+      <p>Name: </p>
+      <input type="text" class="py-2 w-2/3 text-green-600" v-model="customerName"/>
+      <button class="bg-green-300 py-1 px-1 rounded " @click="bookRoom(customerName)">book room</button>
+    </div>
 
+
+    <br><br><br>
     <p>Verfügbare Zimmer: {{ zimmerListe.length}}</p>
-    <div v-for="room in zimmerListe" :key="room.id">
-      <RoomDetails :roomObject="room" ></RoomDetails>
+    <p v-if="selectedRooms.length > 0">Selected Zimmer: {{ selectedRooms.length}}</p>
+    <p v-else>Keine Zimmer Selected</p>
+    <div v-for="(room, index) in zimmerListe" :key="room.id">
+      <RoomDetails :roomObject="room" :roomIndex="index" @room-index="toggleId" @selectroom-index="selectRoom"></RoomDetails>
     </div>
   </div>
 </template>
@@ -26,14 +35,36 @@ export default {
   data() {
     return {
       zimmerListe: [
-        {id: 1, beds: 4},
-        {id: 2, beds: 2},
-        {id: 3, beds: 3},
-        {id: 4, beds: 4},
-        {id: 5, beds: 2},
-        {id: 6, beds: 41},
-        {id: 7, beds: 41},
-      ]
+        {id: 1, beds: 4, clicked: 0, selected: false},
+        {id: 2, beds: 2, clicked: 0, selected: false},
+        {id: 3, beds: 3, clicked: 0, selected: false},
+        {id: 4, beds: 4, clicked: 0, selected: false},
+        {id: 5, beds: 2, clicked: 0, selected: false},
+        {id: 6, beds: 4, clicked: 0, selected: false},
+        {id: 7, beds: 4, clicked: 0, selected: false},
+      ],
+      customerName: ""
+    }
+  }, methods: {
+    toggleId(index) {
+      this.zimmerListe[index].clicked++;
+    },
+    selectRoom(index) {
+      this.zimmerListe[index].selected=!this.zimmerListe[index].selected;
+    },
+    bookRoom(customerName) {
+      if(this.customerName.trim() === ""){
+        return;
+      }
+      console.log("room booked for customer " + customerName);
+    }
+  },
+  computed: {
+    selectedRooms() {
+      const selectedRooms = this.zimmerListe.filter( (room) => {
+        return room.selected;
+      });
+      return selectedRooms;
     }
   }
 }
--- boutique-hotel-technikum-vue/src/components/RoomDetails.vue
+++ boutique-hotel-technikum-vue/src/components/RoomDetails.vue
@@ -2,20 +2,30 @@
 
 export default {
   name: 'RoomDetails',
-  props: ["roomObject"],
+  props: ["roomObject", "roomIndex"],
   data() {
     return {
       room: this.roomObject
     }
   },
-  methods: {}
+  methods: {
+    toggleId() {
+      this.$emit("room-index", this.roomIndex)
+    },
+    selectRoom() {
+      this.$emit("selectroom-index", this.roomIndex)
+    }
+  }
 }
 </script>
 
 <template>
-  <div class="flex justify-between py-2 px-2 items-center">
+  <div class="flex justify-between py-2 px-2 items-center cursor-pointer"
+       :class="{'bg-green-600': this.room.selected}"
+        @click="toggleId">
     <p>Id: {{room.id}}</p>
     <p>Number of beds: {{ room.beds}}</p>
-    <button class="bg-green-300 py-1 px-1 rounded">book room</button>
+    <p>Clicked: {{room.clicked}}</p>
+    <button class="bg-green-300 py-1 px-1 rounded " @click="selectRoom">select room</button>
   </div>
 </template>
