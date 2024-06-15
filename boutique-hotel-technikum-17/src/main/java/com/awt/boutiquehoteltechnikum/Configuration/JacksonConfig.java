eddf1f9dc54d173c4fa50691e5e5720f2ff2af89
Leo <Leo@DESKTOP-HBBE94K>
2024-06-15 18:00:04 +0200

    added custom object mapper

---
+++ boutique-hotel-technikum-17/src/main/java/com/awt/boutiquehoteltechnikum/Configuration/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.awt.boutiquehoteltechnikum.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, true);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
+
