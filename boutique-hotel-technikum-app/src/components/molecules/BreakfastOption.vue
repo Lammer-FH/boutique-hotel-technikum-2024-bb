c2f335dc40430aa71e847a803db68745ad04814e
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-06-16 22:15:29 +0200

    Added BreakfastOption molecule

---
+++ boutique-hotel-technikum-app/src/components/molecules/BreakfastOption.vue
@@ -0,0 +1,22 @@
+<template>
+    <ion-label :position="position">Include Brea</ion-label>
+    <ion-radio-group v-bind="includeBreakfast" @ionChange="$emit('update:includeBreakfast', includeBreakfast)">
+        <radio-button label="Yes" value="yes"></radio-button>
+        <radio-button label="No" value="no"></radio-button>
+    </ion-radio-group>
+</template>
+  
+<script>
+    import RadioButton from '@/components/atoms/RadioButton.vue';
+    
+    export default {
+        name: 'BreakfastOption',
+        components: {
+            RadioButton
+        },
+        props: {
+            includeBreakfast: String
+        }
+    };
+</script>
+  
