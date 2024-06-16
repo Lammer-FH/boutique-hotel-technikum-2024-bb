bc5cbf9b98513bb714ce673fc50158f4ad6a1eb5
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-06-16 22:16:37 +0200

    added payment page route

--- boutique-hotel-technikum-app/src/router/index.ts
+++ boutique-hotel-technikum-app/src/router/index.ts
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
+import PaymentPage from '../views/PaymentPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/payment'
   },
   {
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/payment',
+    name: 'Payment',
+    component: PaymentPage
   }
 ]
 
