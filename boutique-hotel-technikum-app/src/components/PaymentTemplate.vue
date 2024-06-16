a373600f8752453e8896033c29acba7701401b5f
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-06-16 22:16:15 +0200

    added payment template

---
+++ boutique-hotel-technikum-app/src/components/PaymentTemplate.vue
@@ -0,0 +1,32 @@
+<template>
+    <BookingDetailsOverveiw
+        :fromDate="fromDate"
+        :toDate="toDate"
+        :nights="nights"
+        :guests="guests"
+        :room="room"
+        :total="total"
+    />
+    <PersonalInformationForm />
+</template>
+
+<script>
+import BookingDetailsOverveiw from '@/components/organisms/BookingDetailsOverview.vue';
+import PersonalInformationForm from '@/components/organisms/PersonalInformationForm.vue';
+
+export default {
+    name:"PaymentTemplate",
+    components: {
+        BookingDetailsOverview,
+        PersonalInformationForm,
+    },
+    props: {
+        fromDate: String,
+        toDate: String,
+        nights: Number,
+        guests: String,
+        room: String,
+        total: String
+    }
+};
+</script>
