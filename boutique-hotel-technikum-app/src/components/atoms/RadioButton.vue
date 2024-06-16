2eb7893d6ac23a6a0c30ea20d11ef69a085ea901
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-06-16 22:14:26 +0200

    added InputField and RadioButton atoms

---
+++ boutique-hotel-technikum-app/src/components/atoms/RadioButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <ion-item>
+        <ion-label>{{ label }}</ion-label>
+        <ion-radio slot="start" :value="value"></ion-radio>
+    </ion-item>
+</template>
+  
+<script>
+    export default {
+        name: 'RadioButton',
+        props: {
+        label: String,
+        value: String
+        }
+    };
+</script>
+  
