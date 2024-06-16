09bf9265512994744d59ff609f27e91542d778aa
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-06-16 22:16:07 +0200

    added BookingDetails and PersonalInformation organisms

---
+++ boutique-hotel-technikum-app/src/components/organisms/BookingDetailsOverview.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="details">
+        <p>Dates: <strong>From: {{ fromDate }} To: {{ toDate }}</strong></p>
+        <p>Nights: <strong>{{ nights }}</strong></p>
+        <p>Guests: <strong>{{ guests }}</strong></p>
+        <p>Room: <strong>{{ room }}</strong></p>
+        <p>Total: <strong>{{ total }}</strong></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BookingDetailsOverview",
+    props: {
+        fromDate: String,
+        toDate: String,
+        nights: Number,
+        guests: String,
+        room: String,
+        total: String
+    }
+};
+</script>
+
+<style scoped>
+    .details p {
+        margin: 5px 0;
+    }
+</style>
---
+++ boutique-hotel-technikum-app/src/components/organisms/PersonalInformationForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <ion-label>Personal Information</ion-label>
+    <form @submit.prevent="submitForm">
+        <input-field label="Name" v-model="name"></input-field>
+        <input-field label="Surname" v-model="surname"></input-field>
+        <input-field label="Email" type="email" v-model="email"></input-field>
+        <input-field label="Confirm Email" type="email" v-model="confirmEmail"></input-field>
+        <breakfast-option v-model="includeBreakfast"></breakfast-option>
+        <ion-button shape="round" type="submit">
+            Order
+            <ion-icon slot="end" :icon="chevronForward "></ion-icon>
+        </ion-button>
+    </form>
+</template>
+
+<script>
+import { IonButton, IonIcon } from '@ionic/vue';
+import { chevronForward } from 'ionicons/icons';
+
+import InputField from '@/components/atoms/InputField.vue';
+import BreakfastOption from '@/components/molecules/BreakfastOption.vue';
+
+export default {
+    name: 'PersonalInformationForm',
+    components: {
+        IonButton,
+        IonIcon,
+        InputField,
+        BreakfastOption
+    },
+    data() {
+        return {
+        name: '',
+        surname: '',
+        email: '',
+        confirmEmail: '',
+        includeBreakfast: 'yes'
+        };
+    },
+    methods: {
+        submitForm() {
+        if (this.email !== this.confirmEmail) {
+            alert('Emails do not match!');
+            return;
+        }
+        alert('Form submitted!');
+        }
+    }
+};
+</script>
