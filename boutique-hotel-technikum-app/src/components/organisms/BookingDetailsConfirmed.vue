e2d523018aa361f9e67d87f75fc35551711d3b77
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-06-17 10:39:37 +0200

    added organisms for booking confirmation page

---
+++ boutique-hotel-technikum-app/src/components/organisms/BookingDetailsConfirmed.vue
@@ -0,0 +1,23 @@
+<template>
+    <ion-card>
+        <ion-card-content>
+            <BookingDetail label="Booking-Number">1234</BookingDetail>
+            <BookingDetail label="Status">Confirmed</BookingDetail>
+            <BookingDetail label="Check-in">July 06, 2024</BookingDetail>
+            <BookingDetail label="Check-out">July 07, 2024</BookingDetail>
+            <BookingDetail label="Total">0,00€</BookingDetail>
+            <BookingDetail label="Breakfast">Included</BookingDetail>
+        </ion-card-content>
+    </ion-card>
+</template>
+
+<script>
+import BookingDetail from '../molecules/BookingDetail.vue';
+
+export default {
+    name: 'BookingDetailsConfirmed',
+    components: {
+        BookingDetail
+    }
+};
+</script>
