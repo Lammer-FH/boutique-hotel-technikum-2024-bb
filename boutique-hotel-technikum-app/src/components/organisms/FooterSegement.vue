5d3b076b6994436f60a7659d050046a5fc8b5f79
Alice Mihalca <se23m042@technikum-wien.at>
2024-06-17 16:05:24 +0200

    added FooterSegment organism

---
+++ boutique-hotel-technikum-app/src/components/organisms/FooterSegement.vue
@@ -0,0 +1,25 @@
+<template>
+  <ion-footer>
+    <ion-toolbar>
+      <div class="footer-content">
+        <ion-icon name="information-circle-outline" size="large"></ion-icon>
+        <div class="footer-links">
+          <ion-button fill="clear" href="/about-us">About us</ion-button>
+          <ion-button fill="clear" href="/contact">Contact</ion-button>
+          <ion-button fill="clear" href="/location">Location</ion-button>
+        </div>
+        <p>&copy; Copyright Boutique-Technikum. All rights reserved.</p>
+      </div>
+    </ion-toolbar>
+  </ion-footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterSegment'
+};
+</script>
+
+<style scoped>
+
+</style>
