b0314c7f296cfa732c11a3012e705d1e4f2f656e
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-06-18 22:55:21 +0200

    added get room logic

--- boutique-hotel-technikum-app/src/components/BookingConfirmationTemplate.vue
+++ boutique-hotel-technikum-app/src/components/BookingConfirmationTemplate.vue
@@ -5,25 +5,30 @@
             <p class="ion-text-center">We are pleased to inform you that your reservation request has been received and confirmed. Thank you!</p>
             <h4>Booking Details</h4>
             <BookingDetailsConfirmed />
-            <RoomCard/>
+            <RoomCard :roomObject="room" />
             <h4>Personal Information</h4>
             <PersonalInformationCard />
-            <Contact />
+            <FooterSegment />
         </ion-content>
     </ion-page>
 </template>
 
 <script>
 import BookingDetailsConfirmed from '@/components/organisms/BookingDetailsConfirmed.vue';
+import RoomCard from './molecules/RoomCard.vue';
 import PersonalInformationCard from '@/components/organisms/PersonalInformationCard.vue';
-import Contact from '@/components/organisms/Contact.vue';
+import FooterSegment from './FooterSegment.vue';
 
 export default {
     name:"BookingConfirmationTemplate",
     components: {
         BookingDetailsConfirmed,
+        RoomCard,
         PersonalInformationCard,
-        Contact
+        FooterSegment
+    },
+    props: {
+        room: {}
     }
 };
 </script>
