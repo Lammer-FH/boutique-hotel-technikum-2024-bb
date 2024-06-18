fe67c616e17fb560fd340a7136613035bb397479
sem-afor <se23m047@technikum-wien.at>
2024-06-18 11:15:23 +0200

    room store aus room list in search rooms template gehoben

--- boutique-hotel-technikum-app/src/components/SearchRoomsTemplate.vue
+++ boutique-hotel-technikum-app/src/components/SearchRoomsTemplate.vue
@@ -1,13 +1,36 @@
-<script setup lang="ts">
-
-import SearchForRoomsCard from "@/components/organisms/SearchForRoomsCard.vue";
-import ListOfRooms from "@/components/organisms/ListOfRooms.vue";
-</script>
-
 <template>
   <SearchForRoomsCard></SearchForRoomsCard>
-  <ListOfRooms></ListOfRooms>
+  <ListOfRooms :room-list="roomList"></ListOfRooms>
 </template>
+
+<script lang="ts">
+import RoomCard from "@/components/molecules/RoomCard.vue";
+import {useRoomsStore} from "@/stores/room";
+import {onMounted, ref} from "vue";
+import SearchForRoomsCard from "@/components/organisms/SearchForRoomsCard.vue";
+import ListOfRooms from "@/components/organisms/ListOfRooms.vue";
+
+export default {
+  components: {ListOfRooms, SearchForRoomsCard, RoomCard},
+  setup() {
+    const roomStore = useRoomsStore();
+    const roomList = ref<[]>([]);
+
+    onMounted(async () => {
+      await roomStore.fetchRooms();
+      roomList.value = roomStore.rooms;
+    });
+    console.log(roomList.value)
+    return { roomList };
+  },
+  data() {
+    return {
+      // todo get actual room list
+      roomStore: useRoomsStore(),
+    }
+  }
+}
+</script>
 
 <style scoped>
 
