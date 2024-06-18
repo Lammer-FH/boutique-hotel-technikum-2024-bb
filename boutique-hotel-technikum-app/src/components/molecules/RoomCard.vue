c708939e19b8239b9a25f12a0ae210bc3e0797e9
sem-afor <se23m047@technikum-wien.at>
2024-06-18 16:49:00 +0200

    add icons with room extras

--- boutique-hotel-technikum-app/src/components/molecules/RoomCard.vue
+++ boutique-hotel-technikum-app/src/components/molecules/RoomCard.vue
@@ -6,16 +6,13 @@
       <ion-card-subtitle>{{room.price}}/Night</ion-card-subtitle>
     </ion-card-header>
 
-      <ion-card-content>
+      <ion-card-content >
         <p>Beds: {{ room.bedCount}}</p>
-        <ion-icon src="./icons/bathtub.svg"></ion-icon>
-        <ion-icon src="./icons/hairdryer.svg"></ion-icon>
-        <ion-icon src="./icons/balcony.svg"></ion-icon>
-        <ion-icon src="./icons/coffeemachine.svg"></ion-icon>
-        <ion-icon src="./icons/watercooker.svg"></ion-icon>
-        <ion-icon src="./icons/wifi.svg"></ion-icon>
-
-
+        <span class="style-children">
+          <div v-if="iconPathList.length > 0" v-for="(icon, index) in iconPathList" :key="icon">
+            <ion-icon class="right-padding" :src="icon"></ion-icon>
+          </div>
+        </span>
         <ion-button shape="round" @click="selectRoom">Select Room</ion-button>
       </ion-card-content>
   </ion-card>
@@ -24,6 +21,9 @@
 <script lang="ts">
 import RoomImage from "@/components/atoms/RoomImage.vue";
 import { IonIcon } from '@ionic/vue';
+import {useRoomExtraStore} from "@/stores/roomExtras";
+import {ref} from "vue";
+import {useExtraTypeStore} from "@/stores/extraType";
 
 export default {
   name: 'RoomCard',
@@ -31,14 +31,44 @@ export default {
   props: ["roomObject", "roomIndex"],
   data() {
     return {
-      room: this.roomObject
+      room: this.roomObject,
+      roomExtraStore: useRoomExtraStore(),
+      extraTypeStore: useExtraTypeStore(),
+      extraList: ref<any[]>([]),
+      iconPathList: ['']
     }
+  },
+  async mounted() {
+      await this.getRoomExtras();
+      this.getIconLinks();
   },
   methods: {
     selectRoom() {
       console.log("select room triggered for room id " + this.roomObject.id)
       this.$emit("selectroom", this.roomIndex)
+    },
+    async getRoomExtras() {
+      await this.roomExtraStore.fetchRoomExtrasForRoom(this.room.id);
+      this.extraList = this.roomExtraStore.roomExtras;
+      console.log("extra list for id = " + this.room.id);
+      console.log(this.extraList)
+    },
+    getIconLinks(){
+      this.extraList.forEach((extra) => {
+        // Perform your "before each" action here
+        // For example, logging each item before getting its icon link
+        console.log('Processing extra:', extra);
+
+        // Then get the icon link for each extra
+        const iconLink = this.getIconPathForExtra(extra.title);
+        this.iconPathList.push(iconLink.toString());
+      });
+    },
+    getIconPathForExtra(extra){
+      console.log("in path extra = " + extra);
+      return './icons/' + extra + ".svg";
     }
+
   }
 }
 </script>
@@ -53,6 +83,10 @@ ion-card.style-children {
   display: flex;
   flex-direction: row;
 }
+span.style-children {
+  display: flex;
+  flex-direction: row;
+}
 span.style-columns {
   display: flex;
   flex-direction: row;
@@ -60,4 +94,11 @@ span.style-columns {
 img.image-width {
   width: 30%;
 }
+ion-icon.right-padding {
+  padding-right: 0.25rem;
+}
 </style>
+
+
+
+
