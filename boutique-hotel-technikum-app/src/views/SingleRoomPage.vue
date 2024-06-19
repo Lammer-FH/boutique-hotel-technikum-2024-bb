ef766eb7f2a7e3999707252d95576d7e71437002
sem-afor <se23m047@technikum-wien.at>
2024-06-19 14:40:39 +0200

    broken trying t fix

--- boutique-hotel-technikum-app/src/views/SingleRoomPage.vue
+++ boutique-hotel-technikum-app/src/views/SingleRoomPage.vue
@@ -5,23 +5,73 @@
       </ion-toolbar>
     </ion-header>
     <ion-content >
-      <SingleRoomTemplate :roomId="id" :startDate="startDate" :endDate="endDate"/>
+      <SingleRoomTemplate :room="roomObject[0]" :startDate="startDate" :endDate="endDate"/>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import SingleRoomTemplate from "@/components/SingleRoomTemplate.vue";
+import {useRoomsStore} from "@/stores/room";
+import {ref, watch} from "vue";
 import { useRoute } from 'vue-router';
-import { watch } from 'vue';
 
-const route = useRoute();
+export default {
+  components: {SingleRoomTemplate},
+  data() {
+    return {
+      roomStore: useRoomsStore(),
+      roomObject: ref<any[]>([]),
+    }
+  },
+  setup() {
+    const route = useRoute();
 
-const id = route.query.id;
-const startDate = route.query.start;
-const endDate = route.query.end;
-watch(() => route.path, () => {});
+    const roomId = route.query.id;
+    const startDate = route.query.start;
+    const endDate = route.query.end;
+    watch(() => route.path, () => {});
 
+    return {
+      roomId,
+      startDate,
+      endDate
+    }
+  },
+  beforeRouteUpdate(to, from, next) {
+    // Fetch data based on the new route parameters (e.g., to.params.itemId)
+    this.fetchRoomById(to.params.id[0]);
+    next();
+  },
+  async mounted() {
+    if (this.roomId) {
+      //await this.fetchRoomById(this.roomId[0]);
+
+    } else {
+      console.error("No room ID provided, cannot fetch room details.");
+    }
+  },
+  created() {
+    const reloaded = localStorage.getItem('reloaded');
+    if (reloaded !== 'true') {
+      localStorage.setItem('reloaded', 'true');
+      location.reload();
+    }
+  },
+  methods: {
+    async fetchRoomById(roomId: string) {
+      const newRoomId = parseInt(roomId);
+      if (!newRoomId) {
+        console.error("Room ID is required to fetch room details.");
+        return;
+      }
+      console.log(`Fetching room details for ID: ${newRoomId}`);
+      this.roomObject.splice(0);
+      console.log("got room fetch " + newRoomId)
+      this.roomObject.push(await this.roomStore.fetchRoomById(newRoomId));
+    },
+  }
+}
 </script>
 
 <style scoped>
