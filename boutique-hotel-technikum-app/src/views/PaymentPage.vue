e69af349dc4052a449b6008e7501d2b53c13a939
Kate Audrey Ongcoy <se23m034@technikum-wien.at>
2024-06-19 11:06:47 +0200

    added error handling for form

--- boutique-hotel-technikum-app/src/views/PaymentPage.vue
+++ boutique-hotel-technikum-app/src/views/PaymentPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <form>
+    <form @submit.prevent="saveBooking">
         <PaymentTemplate 
             :fromDate="fromDate"
             :toDate="toDate"
@@ -22,10 +22,17 @@
             @update:confirmEmail="updateConfirmEmail"
             @update:includeBreakfast="updateIncludeBreakfast"
         />
-        <ion-button shape="round"  @click.prevent="saveBooking">
+        <ion-button shape="round" type="submit" >
             Order
             <ion-icon slot="end" :icon="chevronForward "></ion-icon>
         </ion-button>
+        <ion-alert 
+            :is-open="showAlert"
+            :header="alertHeader"
+            :message="alertMessage"
+            :buttons="alertButton"
+            @didDismiss="showAlert = false">
+        </ion-alert>
     </form>
 </template>
 
@@ -33,6 +40,7 @@
 import PaymentTemplate from '@/components/PaymentTemplate.vue';
 import { useCustomerStore } from '@/stores/customer'
 import {useBookingStore} from "@/stores/booking";
+import {useRoomsStore} from "@/stores/room";
 
 export default {
     name: 'PaymentPage',
@@ -43,6 +51,7 @@ export default {
         return {
             customer: useCustomerStore(),
             bookings: useBookingStore(),
+            roomStore: useRoomsStore(),
             fromDate: "2024-07-06",
             toDate: "2024-07-07",
             nights: 1,
@@ -55,7 +64,11 @@ export default {
             address: '',
             email: '',
             confirmEmail: '',
-            includeBreakfast: 'yes'
+            includeBreakfast: 'yes',
+            showAlert: false,
+            alertHeader: '',
+            alertMessage: '',
+            alertButton: ['OK']
         }
     },
     methods: {
@@ -68,9 +81,18 @@ export default {
         updateIncludeBreakfast(value) { this.includeBreakfast = value; },
         async saveBooking() {
             try {
+                // Validation
+                if (!this.name || !this.surname || !this.phoneNumber || !this.address || !this.email || !this.confirmEmail) {
+                    throw new Error('All fields are required.');
+                }
+                if (this.email !== this.confirmEmail) {
+                    throw new Error('Emails do not match.');
+                }
+
                 let guest = await this.saveGuest();
                 this.createBooking(guest);
             } catch (error) {
+                this.showError(error.message);
                 console.error("Error saving booking:", error);
             }
         },
@@ -83,6 +105,11 @@ export default {
             let totalCost = parseFloat(this.total)
             let booking = await this.bookings.createBooking(this.room,"notes",guest.id,1,this.fromDate,this.toDate,breakfastOption,totalCost)
             console.log(booking);
+        },
+        showError(message) {
+            this.alertHeader = 'Error';
+            this.alertMessage = message;
+            this.showAlert = true;
         },
     }
 };
