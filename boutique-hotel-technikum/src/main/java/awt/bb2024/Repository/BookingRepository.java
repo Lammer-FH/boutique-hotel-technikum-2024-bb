1ab4245af7ff7bad1e879b99152ff89920eda331
Leonard Li <Leonard.Li@phactum.at>
2024-06-19 15:49:10 +0200

    deleted java 11 backend and vue app

--- boutique-hotel-technikum/src/main/java/awt/bb2024/Repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package awt.bb2024.Repository;
-
-import awt.bb2024.Model.Booking;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends CrudRepository<Booking, Integer> {
-}
